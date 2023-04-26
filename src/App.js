9bb61f81f1b02348d8162c5f581f4ea35f726730
rehankhan1998 <rehanarham625@gmail.com>
2023-04-26 15:35:29 +0530

    first todo commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState } from "react";
 function App() {
+  const [data, setData] = useState("");
+  const [getItem, setGetItem] = useState([]);
+  const [edit, setEdit] = useState(false);
+
+  const OnchangeHandler = (e) => {
+    setData(e.target.value);
+  };
+  console.log(data);
+  const OnClickHandler = () => {
+    setGetItem((val) => {
+      return [...val, data];
+    });
+    setData("");
+  };
+  const onRemove = (v, i) => {
+    const delets = getItem.filter((v, index) => {
+      return index !== i;
+    });
+    setGetItem(delets);
+  };
+  const oneditHandler = (v, i) => {
+    setEdit(i);
+    setData(v);
+  };
+
+  const onUpdateHanlder = () => {
+    const update = getItem.map((v, i) => {
+      return edit === i ? data : v;
+    });
+    setGetItem(update);
+    setData("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App py-5">
+      <h1 className="fw-bold">Todo</h1>
+      <input type="text" value={data} onChange={OnchangeHandler} />
+      <button
+        disabled={data.length === 0}
+        className="mx-2 btn btn btn-dark"
+        onClick={OnClickHandler}
+      >
+        Add
+      </button>
+      <button className="btn btn btn-dark" onClick={onUpdateHanlder}>
+        update
+      </button>
+      <ol>
+        {getItem.map((v, i) => {
+          return (
+            <>
+              <li className="text-white fs-4 fw-400">{v}</li>
+              <button
+                className="mx-2 btn btn btn-dark"
+                onClick={() => oneditHandler(v, i)}
+              >
+                edit
+              </button>
+
+              <button className="btn btn btn-dark" onClick={() => onRemove(i)}>
+                Delete
+              </button>
+            </>
+          );
+        })}
+      </ol>
     </div>
   );
 }
